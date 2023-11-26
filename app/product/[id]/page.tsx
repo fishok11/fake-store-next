bd59de2ae00dc700c6659f5df9f3ee22b16d3ea8
fishok11 <svllsd811@gmail.com>
2023-11-26 20:20:27 +0300

    counter products render without ssr

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,12 +1,11 @@
 import { Product } from "@/types";
 import styles from './ProductPage.module.scss';
 import CounterProducts from "@/components/CounterProducts/CounterProducts";
-import { getProduct } from "@/services/requests";
+import { getProduct, getProducts } from "@/services/requests";
 import { Metadata } from "next";
+import dynamic from "next/dynamic";
 
-export const metadata: Metadata = {
-  title: 'Product',
-}
+const NoSSR = dynamic(() => import('@/components/CounterProducts/CounterProducts'), { ssr: false })
 
 type ProductProps = {
   params: {
@@ -15,30 +14,37 @@ type ProductProps = {
 }
 
 const ProductPage = async({ params: { id } }: ProductProps) => {
-  const product: Product | undefined | null = await getProduct(id);
-
-  if (product === undefined || product === null) {
-    return null;
-  }
+  const product = await getProduct(id);
   
   return (
       <div className={styles.container}>
         <div className={styles.productContainer}>
           <div className={styles.imageContainer}>
-            <img src={product.image} alt="" className={styles.image}/>
+            <img src={product?.image} alt="" className={styles.image}/>
           </div>
           <div>
             <div className={styles.titleContainer}>
-              <p className={styles.title}>{product.title}</p>
-              <p className={styles.rating}>{product.rating.rate}/{product.rating.count}</p>
+              <p className={styles.title}>{product?.title}</p>
+              <p className={styles.rating}>{product?.rating.rate}/{product?.rating.count}</p>
             </div>
-            <CounterProducts price={product.price} id={id} />
+            <NoSSR price={product?.price} id={id} />
           </div>
-          <p className={styles.description}>{product.description}</p>
+          <p className={styles.description}>{product?.description}</p>
         </div>
-        {/* <p className={styles.container}>{state.product.category}</p> */}
       </div>
   )
+}
+
+export async function generateStaticParams() {
+  const products = await getProducts();
+ 
+  return products?.map((product) => ({
+    slug: product.id,
+  }))
+}
+
+export const metadata: Metadata = {
+  title: 'Product',
 }
 
 export default ProductPage;
--- components/CounterProducts/CounterProducts.tsx
+++ components/CounterProducts/CounterProducts.tsx
@@ -1,19 +1,20 @@
 "use client"
 
+import Loading from '@/app/loading';
 import styles from '@/components/CounterProducts/CounterProducts.module.scss'
 import { addProductToCart } from '@/services/requests';
 import { ProductInCart } from '@/types';
 import { getCookie } from 'cookies-next';
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 type CounterProductsProps = {
-  price: number;
+  price: number | undefined;
   id: string;
 }
 
 const CounterProducts: FC<CounterProductsProps> = ({ price, id }) => {
-  const cartId = getCookie('cart');
- const [count, setCount] = useState(1);
+  const cartId = getCookie('cart')?.toString();
+  const [count, setCount] = useState(1);
   const decrease = () => {
     if (count > 1) {
       setCount(count => count - 1);
@@ -22,19 +23,24 @@ const CounterProducts: FC<CounterProductsProps> = ({ price, id }) => {
   const increase = () => {
     setCount(count => count + 1);
   };
-
   const product: ProductInCart = {
     productId: id,
     quantity: count,
   }
 
-  if (cartId === undefined) { 
+  if (cartId === undefined) {
     return (
-      <h1>
-        Log in to add to cart
-      </h1>
+      <h2 className={styles.actionContainer}>
+        Log in to view the shopping cart
+      </h2>
     )
-  };
+  }
+
+  if (price === undefined) {
+    return (
+      <Loading />
+    )
+  }
   
   return (
     <div className={styles.actionContainer}>
@@ -44,7 +50,7 @@ const CounterProducts: FC<CounterProductsProps> = ({ price, id }) => {
         <div className={styles.counterSimbol}>{count}</div>
         <button className={styles.counterSimbol} onClick={() => increase()}>+</button>
       </div>
-      <button className={styles.button} onClick={() => addProductToCart(cartId.toString(), product)}>Add to cart</button>
+      <button className={styles.button} onClick={() => addProductToCart(cartId, product)}>Add to cart</button>
     </div> 
 )
 
